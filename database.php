55b4d80b853e161df400747593eeaa50898b9a2c
Teenkung123 <51858378+Teenkung123@users.noreply.github.com>
2023-01-15 16:28:21 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php 
 
-$user = "root";
-$host = "localhost";
-$pass = "";
+$user = "info";
+$host = "140.99.98.103";
+$pass = "ritza@555999***";
 $data = "info";
 
 $con = mysqli_connect($host, $user, $pass, $data);
